3b33edf28f9483b28c9a1b7e4baea94b8a1e14f2
saumik patel <saumikpatel32@gmail.com>
2020-09-09 14:03:39 -0400

    apartment marker on map using static data

--- app/src/main/java/com/example/rentals/MapFragment.java
+++ app/src/main/java/com/example/rentals/MapFragment.java
@@ -1,10 +1,19 @@
 package com.example.rentals;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
@@ -21,9 +30,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.util.MapUtils;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -33,17 +47,24 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.TypeFilter;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.maps.android.ui.IconGenerator;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -58,48 +79,96 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     Location mLastLocation;
     Marker mCurrLocationMarker;
     FusedLocationProviderClient mFusedLocationClient;
+    boolean boundary = false;
+
 
     public MapFragment() {
         // Required empty public constructor
     }
 
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        mapFrag = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFrag.getMapAsync(this);
+
 
         Places.initialize(getActivity().getApplicationContext(), "AIzaSyDzkhBJZpa16X7NMsbveeggrcSGfT-IsH0");
 
         // Create a new PlacesClient instance
         PlacesClient placesClient = Places.createClient(getActivity());
         // Initialize the AutocompleteSupportFragment.
-        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+        final AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
                 getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
 
+
         // Specify the types of place data to return.
-        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG, Place.Field.VIEWPORT, Place.Field.ADDRESS_COMPONENTS));
+        //autocompleteFragment.setTypeFilter(TypeFilter.CITIES);
 
         // Set up a PlaceSelectionListener to handle the response.
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @SuppressLint("UseCompatLoadingForDrawables")
             @Override
             public void onPlaceSelected(@NotNull Place place) {
                 // TODO: Get info about the selected place.
-                Log.i("", "Place: " + place.getName());
-                
+                Log.i("", "Place: " + place.getAddressComponents());
+                View fView = autocompleteFragment.getView();
+                EditText etTextInput = fView.findViewById(R.id.places_autocomplete_search_input);
+                etTextInput.setTextColor(Color.BLACK);
+                etTextInput.setHintTextColor(Color.GRAY);
+                etTextInput.setTextSize(14.5f);
+
+
+                ImageView ivClear = fView.findViewById(R.id.places_autocomplete_search_button);
+                ImageButton close = fView.findViewById(R.id.places_autocomplete_clear_button);
+                ivClear.setVisibility(View.GONE);
+                close.setVisibility(View.GONE);
 
                 putmarker(place.getLatLng());
+                LatLng latLng1 = new LatLng(45.496049, -73.577975);
+                LatLng latLng2 = new LatLng(45.496185, -73.578688);
+                LatLng latLng3 = new LatLng(45.496813, -73.578334);
+                LatLng latLng4 = new LatLng(45.496824, -73.577363);
+                LatLng latLng5 = new LatLng(45.495797, -73.577626);
+                LatLng address[] = new LatLng[5];
+                address[0] = latLng1;
+                address[1] = latLng2;
+                address[2] = latLng3;
+                address[3] = latLng4;
+                address[4] = latLng5;
+
+
+                for (int i = 0; i < address.length; i++) {
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(address[i]);
+                    //markerOptions.title("Current Position");
+                    markerOptions.alpha(1.0f);
+                    IconGenerator iconFactory = new IconGenerator(getActivity());
+                    iconFactory.setBackground(getResources().getDrawable(R.drawable.marker1));
+                    iconFactory.setTextAppearance(R.style.myStyleText);
+                    markerOptions.icon(BitmapDescriptorFactory.fromBitmap(iconFactory.makeIcon("500$")));
+
+                    markerOptions.anchor(iconFactory.getAnchorU(), iconFactory.getAnchorV());
+
+
+                    Marker m = mGoogleMap.addMarker(markerOptions);
+                    m.setTag("hello");
+
+                    m.setTitle("vvv");
+
+
+                }
 
             }
-
 
             @Override
             public void onError(@NotNull Status status) {
@@ -107,9 +176,12 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 Log.i("", "An error occurred: " + status);
             }
         });
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
-        mapFrag = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mapFrag.getMapAsync(this);
+
+
+    }
+
+
+    private void putapartment(LatLng address) {
 
     }
 
@@ -124,10 +196,13 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        mGoogleMap.setMinZoomPreference(16);
+
+        mGoogleMap.setMaxZoomPreference(22);
+        mGoogleMap.setMinZoomPreference(17);
+
         mLocationRequest = new LocationRequest();
-      //  mLocationRequest.setInterval(30000); // two minute interval
-       // mLocationRequest.setFastestInterval(30000);
+        //  mLocationRequest.setInterval(30000); // two minute interval
+        // mLocationRequest.setFastestInterval(30000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -137,24 +212,30 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 //Location Permission already granted
                 mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
                 mGoogleMap.setMyLocationEnabled(true);
+
+                UiSettings mapsetting = googleMap.getUiSettings();
+                mapsetting.setZoomControlsEnabled(true);
+                mapsetting.setZoomGesturesEnabled(true);
+                mapsetting.setAllGesturesEnabled(true);
+                mapsetting.setScrollGesturesEnabled(true);
+
+
             } else {
                 //Request Location Permission
                 checkLocationPermission();
             }
-        }
-        else {
+        } else {
             mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
             mGoogleMap.setMyLocationEnabled(true);
         }
 
     }
+
     LocationCallback mLocationCallback = new LocationCallback() {
         @RequiresApi(api = Build.VERSION_CODES.O)
         @Override
         public void onLocationResult(LocationResult locationResult) {
             List<Location> locationList = locationResult.getLocations();
-
-
             if (locationList.size() > 0) {
                 //The last location in the list is the newest
                 Location location = locationList.get(locationList.size() - 1);
@@ -165,30 +246,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 }
                 LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
 
-              putmarker(latLng);
+                putmarker(latLng);
 
-                }
-
-
-
-
+            }
         }
     };
 
-    private void putmarker(LatLng latLng) {
-        //Place current location marker
-
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng);
-        markerOptions.title("Current Position");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
-        mCurrLocationMarker = mGoogleMap.addMarker(markerOptions);
-
-        //move map camera
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11));
-    }
-
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
     private void checkLocationPermission() {
         if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -209,18 +274,16 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                                 //Prompt the user once explanation has been shown
                                 ActivityCompat.requestPermissions(getActivity(),
                                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                        MY_PERMISSIONS_REQUEST_LOCATION );
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
                             }
                         })
                         .create()
                         .show();
-
-
             } else {
                 // No explanation needed, we can request the permission.
                 ActivityCompat.requestPermissions(getActivity(),
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_LOCATION );
+                        MY_PERMISSIONS_REQUEST_LOCATION);
             }
         }
     }
@@ -247,13 +310,28 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
-                   // Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                    // Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
                 }
                 return;
             }
-
             // other 'case' lines to check for other
             // permissions this app might request
         }
+    }
+
+    private void putmarker(LatLng latLng) {
+        //Place current location marker
+        if (mCurrLocationMarker != null) {
+            mCurrLocationMarker.remove();
+        }
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Current Position");
+        markerOptions.alpha(0.8f);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
+        mCurrLocationMarker = mGoogleMap.addMarker(markerOptions);
+
+        //move map camera
+        mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11));
     }
 }
