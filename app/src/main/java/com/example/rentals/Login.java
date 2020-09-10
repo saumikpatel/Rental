ec0241e8facca8345e44a7cfe8a4d14d954e399c
sahil saini <sahil7.Saini7@gmail.com>
2020-09-09 21:44:26 -0400

    SIGN UP UI AND FORGOT PASSWORD UI

--- app/src/main/java/com/example/rentals/Login.java
+++ app/src/main/java/com/example/rentals/Login.java
@@ -2,13 +2,38 @@ package com.example.rentals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button create,forgotpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        create=findViewById(R.id.create);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(Login.this,CreateAccount.class);
+                startActivity(i);
+            }
+        });
+        forgotpass=findViewById(R.id.forgotpass);
+        forgotpass.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(Login.this,ForgotPassword.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
