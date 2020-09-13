9c6b4a16864cad5607616bababc4faac211fbd62
Viraj <virajgandhi3@gmail.com>
2020-09-13 01:58:01 -0400

    UI Ad Details and Minor bug fix

--- app/src/main/java/com/example/rentals/Postadd.java
+++ app/src/main/java/com/example/rentals/Postadd.java
@@ -3,26 +3,32 @@ package com.example.rentals;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.textfield.TextInputLayout;
 
 
 public class Postadd extends AppCompatActivity {
 
-    private TextInputLayout textInputLayout;
 
-    private AutoCompleteTextView dropDownText;
+    private AutoCompleteTextView price, unit, bedroom, bathroom, pet , smoke , parking;
+
+     Button  btn_postad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_postadd);
 
-        textInputLayout = findViewById(R.id.textField3);
-        dropDownText = findViewById(R.id.dropdown_text);
+        price = findViewById(R.id.price);
+        unit = findViewById(R.id.unit);
+        bedroom = findViewById(R.id.bedroom);
+        bathroom = findViewById(R.id.bathroom);
+        pet = findViewById(R.id.pet);
+        smoke = findViewById(R.id.smoke);
+        parking = findViewById(R.id.parking);
 
+        /* -------*/
         String[] items = new String[] {
                 "Fixed Rates",
                 "Please Contact"
@@ -34,8 +40,125 @@ public class Postadd extends AppCompatActivity {
                 items
         );
 
-        dropDownText.setAdapter(adapter);
+        price.setAdapter(adapter);
 
+        /* -------*/
+
+        String[] units = new String[] {
+                "Apartment",
+                "Room",
+                "House",
+                "Condo"
+        };
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(
+                Postadd.this,
+                R.layout.dropdown_item,
+                units
+        );
+
+        unit.setAdapter(adapter1);
+
+        /* -------*/
+
+        String[] Bedrooms = new String[] {
+                "Studio",
+                "1",
+                "1 + Den",
+                "2",
+                "2 + Den",
+                "3",
+                "3 + Den",
+                "4",
+                "4 + Den",
+                "5+"
+
+        };
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(
+                Postadd.this,
+                R.layout.dropdown_item,
+                Bedrooms
+        );
+
+        bedroom.setAdapter(adapter2);
+
+        /* -------*/
+
+
+        String[] bathrooms = new String[] {
+                "1",
+                "1.5",
+                "2",
+                "2.5",
+                "3",
+                "3.5",
+                "4",
+                "4.5",
+                "5",
+                "5.5",
+                "6"
+        };
+
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<>(
+                Postadd.this,
+                R.layout.dropdown_item,
+                bathrooms
+        );
+
+        bathroom.setAdapter(adapter3);
+
+        /* -------*/
+
+
+        String[] pets = new String[] {
+                "Yes",
+                "No",
+                "Limited "
+        };
+
+        ArrayAdapter<String> adapter4 = new ArrayAdapter<>(
+                Postadd.this,
+                R.layout.dropdown_item,
+                pets
+        );
+
+        pet.setAdapter(adapter4);
+
+        /* -------*/
+
+
+        String[] smokes = new String[] {
+                "Yes",
+                "No",
+                "Outdoors only "
+        };
+
+        ArrayAdapter<String> adapter5 = new ArrayAdapter<>(
+                Postadd.this,
+                R.layout.dropdown_item,
+                smokes
+        );
+
+        smoke.setAdapter(adapter5);
+
+        /* -------*/
+
+
+        String[] parkings = new String[] {
+                "0",
+                "1",
+                "2",
+                "3+"
+        };
+
+        ArrayAdapter<String> adapter6 = new ArrayAdapter<>(
+                Postadd.this,
+                R.layout.dropdown_item,
+                parkings
+        );
+
+        parking.setAdapter(adapter6);
 
 
     }
