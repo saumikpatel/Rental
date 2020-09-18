84d291c09c0f6888fe1581b0be42ef406702bf4e
Parthiv Patel <51918802+parthiv1503@users.noreply.github.com>
2020-09-18 01:58:34 -0400

    test

--- app/src/main/java/com/example/rentals/ProfileFragment.java
+++ app/src/main/java/com/example/rentals/ProfileFragment.java
@@ -61,4 +61,6 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+    //parthiv
 }
