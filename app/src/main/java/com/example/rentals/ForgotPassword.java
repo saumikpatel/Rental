545e38e22c6768f6a90848dd30fc8b91831c0071
sahil saini <sahil7.Saini7@gmail.com>
2020-09-24 22:05:56 -0400

    reset password firebase code

--- app/src/main/java/com/example/rentals/ForgotPassword.java
+++ app/src/main/java/com/example/rentals/ForgotPassword.java
@@ -1,14 +1,50 @@
 package com.example.rentals;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassword extends AppCompatActivity {
-
+TextInputLayout email;
+Button reset;
+    String emailAddress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
+        email=findViewById(R.id.resetemail);
+        reset=findViewById(R.id.resetpassword);
+
+       emailAddress  = email.getEditText().getText().toString();
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println(emailAddress);
+                auth.sendPasswordResetEmail(emailAddress)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d("", "Email sent.");
+                                }
+                            }
+                        });
+
+            }
+        });
+
+
+
     }
 }
