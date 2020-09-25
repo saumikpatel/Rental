0e2432912291c3b0ecb69ecf87e5780a886e40c4
vidhee <savanividhee@gmail.com>
2020-09-25 18:31:48 -0400

    Code of Make Call to contact owner

--- app/src/main/java/com/example/rentals/Activity/Contact.java
+++ app/src/main/java/com/example/rentals/Activity/Contact.java
@@ -1,28 +1,33 @@
 package com.example.rentals.Activity;
-
+import android.content.Intent;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import android.net.Uri;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.rentals.R;
 
+import org.w3c.dom.Text;
+
 public class Contact extends AppCompatActivity {
     //EditText Username = (EditText) findViewById(R.id.txtname);
     private final static int SEND_SMS_PERMISSION_REQUEST_CODE = 111;
+    private static final int PERMISSION_REQUEST_CODE = 1;
     private Button sendMsg;
-
 
 
     @Override
@@ -30,11 +35,15 @@ public class Contact extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
         sendMsg = (Button) findViewById(R.id.btnsendsms);
-        final TextView phone = (TextView) findViewById(R.id.txtphonenumber);
+
         final EditText msg = (EditText) findViewById(R.id.inputtextsms);
+        final EditText username=(EditText) findViewById(R.id.txtname);
+        final EditText userphone=(EditText) findViewById(R.id.userphone);
+        ImageView imageCall = findViewById(R.id.imgcall);
+        final TextView phone = (TextView) findViewById(R.id.txtphonenumber);
 
         sendMsg.setEnabled(false);
-        //check if permission is not granted
+        //check if permission is granted or not
         if (checkPermission(Manifest.permission.SEND_SMS)) {
             sendMsg.setEnabled(true);
         }   //if permission is not granted then check if the user has denied the permission
@@ -54,6 +63,7 @@ public class Contact extends AppCompatActivity {
                     if (checkPermission(Manifest.permission.SEND_SMS)) {
                         SmsManager smsManager = SmsManager.getDefault();
                         smsManager.sendTextMessage(Phonenumber, null, message, null, null);
+                        Toast.makeText(Contact.this,"Message Sent!",Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(Contact.this, "Permission Denied", Toast.LENGTH_SHORT).show();
                     }
@@ -61,6 +71,14 @@ public class Contact extends AppCompatActivity {
                     Toast.makeText(Contact.this, "Permission Denied", Toast.LENGTH_SHORT).show();
 
                 }
+            }
+        });
+
+
+        imageCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makePhoneCall();
             }
         });
     }//end
@@ -80,7 +98,32 @@ public class Contact extends AppCompatActivity {
                 } else {
                     Toast.makeText(this, "Allow to send an sms!", Toast.LENGTH_LONG).show();
                 }
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    makePhoneCall();
+                } else {
+                    Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+                }
                 break;
         }//swtich end
     }//end
+
+    private void makePhoneCall() {
+        final TextView phone = (TextView) findViewById(R.id.txtphonenumber);
+        String number = phone.getText().toString();
+        if (number.trim().length() > 0) {
+            if (ContextCompat.checkSelfPermission(Contact.this,
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(Contact.this,
+                        new String[]{Manifest.permission.CALL_PHONE}, PERMISSION_REQUEST_CODE);
+            } else {
+                String dial = "tel:" + number;
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+            }
+        } else {
+            Toast.makeText(Contact.this, "Enter Phone Number", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
