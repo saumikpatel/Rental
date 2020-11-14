a2186e012500b95ce6662eff16a8d610a9eb2390
Viraj <virajgandhi3@gmail.com>
2020-11-13 21:40:51 -0500

    Splash screen

--- app/src/main/java/com/example/rentals/Activity/SplashScreen.java
+++ app/src/main/java/com/example/rentals/Activity/SplashScreen.java
@@ -1,10 +1,10 @@
 package com.example.rentals.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.rentals.R;
 
@@ -25,7 +25,7 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },3000);
+        },1500);
 
     }
 }
